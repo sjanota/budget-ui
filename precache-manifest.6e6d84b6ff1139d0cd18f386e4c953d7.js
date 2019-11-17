74cd3602927602b4a7b9c2f6dee6f2eb4d43495b
Szymon Janota <szymon.janota@sap.com>
2019-11-17 16:57:43 +0100

    Updates

---
+++ precache-manifest.6e6d84b6ff1139d0cd18f386e4c953d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42a434ff61ae65d43491933eca542f64",
+    "url": "/budget-ui/index.html"
+  },
+  {
+    "revision": "292858610ae8fe530e0f",
+    "url": "/budget-ui/static/css/2.c297138a.chunk.css"
+  },
+  {
+    "revision": "efee57e22ace27709ff0",
+    "url": "/budget-ui/static/css/main.298473e5.chunk.css"
+  },
+  {
+    "revision": "292858610ae8fe530e0f",
+    "url": "/budget-ui/static/js/2.cbc921a9.chunk.js"
+  },
+  {
+    "revision": "efee57e22ace27709ff0",
+    "url": "/budget-ui/static/js/main.c0fb7e48.chunk.js"
+  },
+  {
+    "revision": "33c730efa5f72939ea01",
+    "url": "/budget-ui/static/js/runtime~main.cf4fc8a1.js"
+  }
+]);
