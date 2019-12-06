a6d6f8d6b203f1c19420889947270cd1ff5fab5d
Szymon Janota <szymon.janota@sap.com>
2019-12-06 21:56:03 +0100

    Add tests for ExpensesPage and TransfersPage

---
+++ src/components/Transfers/TransfersPage.test.js
@@ -0,0 +1,26 @@
+import { MockedProvider } from '@apollo/react-testing';
+import { render, waitForDomChange } from '@testing-library/react';
+import React from 'react';
+
+import dict from '../../lang/pl';
+import { BudgetContext } from '../gql/budget';
+import { DictionaryContext } from '../sbadmin2/language';
+import { TransfersPage } from './TransfersPage';
+
+const selectedBudget = {
+  id: 'f834e106-dc2f-4d71-a7b4-476f531891e7',
+  name: 'my budget',
+};
+
+it('Accounts', async () => {
+  const { container } = render(
+    <DictionaryContext.Provider value={dict}>
+      <MockedProvider mocks={[]}>
+        <BudgetContext.Provider value={{ selectedBudget }}>
+          <TransfersPage />
+        </BudgetContext.Provider>
+      </MockedProvider>
+    </DictionaryContext.Provider>
+  );
+  await waitForDomChange(() => {}, container);
+});
