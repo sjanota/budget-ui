44cf78b8375e582c83f83da8d56216509e520e11
Szymon Janota <szymon.janota@sap.com>
2019-12-07 20:00:20 +0100

    Refactor Combobox to use react-bootstrap

--- src/components/sbadmin2/components/Combobox/Combobox.js
+++ src/components/sbadmin2/components/Combobox/Combobox.js
@@ -2,142 +2,139 @@ import './Combobox.css';
 
 import classnames from 'classnames';
 import PropTypes from 'prop-types';
-import React, { useEffect, useRef, useState } from 'react';
+import React, {
+  forwardRef,
+  useCallback,
+  useEffect,
+  useRef,
+  useState,
+} from 'react';
+import { Dropdown } from 'react-bootstrap';
 
 export default function Combobox({
   disabled,
   allowedValues,
   _ref,
-  defaultValue,
+  defaultValue: defaultID,
   className,
   required,
 }) {
-  const defaultValueObject = allowedValues.find(v => v.id === defaultValue);
+  function valueByID(id) {
+    return allowedValues.find(v => v.id === id);
+  }
+
+  const valueByLabel = useCallback(
+    label => allowedValues.find(v => v.label === label),
+    [allowedValues]
+  );
+
   const [show, setShow] = useState(false);
   const [filter, setFilter] = useState(
-    defaultValueObject ? defaultValueObject.label : ''
+    defaultID ? valueByID(defaultID).label : ''
   );
-  const [selectedIdx, setSelectedIdx] = useState(0);
-  const menuRef = useRef();
-  const classNames = classnames('combobox', 'input-group', className);
-  const filtered = allowedValues.filter(v =>
-    v.label.toLowerCase().includes(filter.toLowerCase())
-  );
-  const wrapperRef = useRef();
+
+  const dropdownRef = useRef();
+
+  const filtered = filter
+    ? allowedValues.filter(v =>
+        v.label.toLowerCase().includes(filter.toLowerCase())
+      )
+    : allowedValues;
 
   useEffect(() => {
     if (_ref) {
-      _ref.current = { value: defaultValue };
+      _ref.current = { value: defaultID };
     }
-  }, [_ref, defaultValue]);
+  }, [_ref, defaultID]);
 
   useEffect(() => {
-    function onClickOutside(event) {
-      if (!wrapperRef.current || wrapperRef.current.contains(event.target)) {
+    function isClickedInside(event) {
+      return !dropdownRef.current || dropdownRef.current.contains(event.target);
+    }
+
+    function handleClickOutside(event) {
+      if (isClickedInside(event)) {
         return;
       }
-
       setShow(false);
-      const selected = allowedValues.find(v => v.label === filter);
-      if (!selected) {
+      if (!valueByLabel(filter)) {
         setFilter('');
       }
     }
-    document.addEventListener('mousedown', onClickOutside);
-    return () => document.removeEventListener('mousedown', onClickOutside);
-  });
+
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => document.removeEventListener('mousedown', handleClickOutside);
+  }, [filter, valueByLabel]);
+
+  function onInputChange(e) {
+    const label = e.target.value;
+    setFilter(label);
+    const selected = valueByLabel(label);
+    if (selected) {
+      _ref.current = { value: selected.id };
+    } else {
+      setShow(true);
+    }
+  }
 
   function onClick(id) {
-    const selectedLabel = allowedValues.find(v => v.id === id).label;
-    setShow(false);
-    setFilter(selectedLabel);
+    const selected = valueByID(id);
+    setFilter(selected.label);
     _ref.current = { value: id };
   }
 
-  function onInputChange(e) {
-    const value = e.target.value;
-    setShow(true);
-    setFilter(value);
-    setSelectedIdx(0);
-    const selected = allowedValues.find(v => v.label === value);
-    if (selected) {
-      _ref.current = { value: selected.id };
-    }
-  }
-
-  function onKeyDown(e) {
-    if (e.keyCode === 40) {
-      // up
-      e.preventDefault();
-      if (selectedIdx >= filtered.length - 1) {
-        setSelectedIdx(0);
-      } else {
-        setSelectedIdx(v => v + 1);
-      }
-    } else if (e.keyCode === 38) {
-      // down
-      e.preventDefault();
-      if (selectedIdx <= 0) {
-        setSelectedIdx(filtered.length - 1);
-      } else {
-        setSelectedIdx(v => v - 1);
-      }
-    } else if (e.keyCode === 13 && show) {
-      // enter
-      e.preventDefault();
-      onClick(filtered[selectedIdx].id);
-    } else if (e.keyCode === 9 && show) {
-      // tab
-      onClick(filtered[selectedIdx].id);
-    }
-  }
-
   return (
-    <div className={classNames} ref={wrapperRef}>
-      <input
-        className='form-control'
-        value={filter}
-        type='text'
-        onChange={onInputChange}
-        onKeyDown={onKeyDown}
-        disabled={disabled}
-        required={required}
-      />
-      <button
-        className='btn btn-secondary dropdown-toggle dropdown-toggle-split no-arrow'
-        data-toggle='dropdown'
-        data-reference='parent'
-        data-flip='false'
-        style={{
-          maxWidth: '2rem',
-          borderTopLeftRadius: 0,
-          borderBottomLeftRadius: 0,
-          margin: -1,
-        }}
-        tabIndex={-1}
-        disabled={disabled}
-        type='button'
-      />
-      <ul
-        ref={menuRef}
-        role='menu'
-        className={classnames('dropdown-menu', { show })}
+    <Dropdown
+      ref={dropdownRef}
+      show={show}
+      onToggle={isOpen => {
+        setShow(isOpen);
+      }}
+    >
+      <Dropdown.Toggle as={Toggle} className={className}>
+        {props => (
+          <>
+            <input
+              className='form-control'
+              value={filter}
+              type='text'
+              onChange={onInputChange}
+              disabled={disabled}
+              required={required}
+            />
+            <button
+              className='btn btn-secondary dropdown-toggle dropdown-toggle-split no-arrow'
+              style={{
+                maxWidth: '2rem',
+                borderTopLeftRadius: 0,
+                borderBottomLeftRadius: 0,
+                margin: -1,
+              }}
+              tabIndex={-1}
+              disabled={disabled}
+              type='button'
+              {...props}
+            />
+          </>
+        )}
+      </Dropdown.Toggle>
+      <Dropdown.Menu
         style={{
           maxHeight: '200px',
           overflowY: 'auto',
         }}
       >
-        {filtered.map((v, idx) => (
-          <li
-            className={`dropdown-item ${idx === selectedIdx ? 'active' : ''}`}
-            onClick={() => onClick(v.id)}
+        {filtered.map(v => (
+          <Dropdown.Item
+            eventKey={v.id}
             key={v.id}
+            onClick={() => onClick(v.id)}
           >
             {v.label}
-          </li>
+          </Dropdown.Item>
         ))}
-      </ul>
-    </div>
+      </Dropdown.Menu>
+    </Dropdown>
   );
 }
 
@@ -154,3 +151,13 @@ Combobox.propTypes = {
   disabled: PropTypes.bool,
   required: PropTypes.bool,
 };
+
+const Toggle = forwardRef(({ className, children, ...props }, ref) => {
+  const classNames = classnames('combobox', 'input-group', className);
+
+  return (
+    <div className={classNames.replace('dropdown-toggle', '')} ref={ref}>
+      {children(props)}
+    </div>
+  );
+});
